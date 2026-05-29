e0d810b51f4494acd7b662cf0dd906c9febae9b1
xpadev <xpadev@gmail.com>
2026-05-29 17:23:55 +0900

    保留#1: Fix migration table name case (FormIntegration) and timestamp

--- packages/database/drizzle/0012_config_json_column_type.sql
+++ packages/database/drizzle/0012_config_json_column_type.sql
@@ -1,2 +1,2 @@
--- Rename config_json to configJson and change type from TEXT to JSON in formIntegration table
-ALTER TABLE `formIntegration` CHANGE `config_json` `configJson` JSON NOT NULL;
+-- Rename config_json to configJson and change type from TEXT to JSON in FormIntegration table
+ALTER TABLE `FormIntegration` CHANGE `config_json` `configJson` JSON NOT NULL;
